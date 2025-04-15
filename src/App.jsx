0db6f195b0a9f74f0994a25032f4cfcc44c66ede
Vinicios Neves <vinicios.neves@mac>
2025-04-15 19:50:54 +0100

    video 1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import './App.css'
 
+// no react, componentes são FUNÇÕES
+
+function FormularioDeEvento () {
+
+  return (
+    <form className="form-evento">
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="nome">
+          Qual o nome do evento?
+        </label>
+        <input type="text" id='nome'/>
+      </fieldset>
+    </form>
+  )
+}
+
 function App() {
   return (
     <main>
@@ -9,6 +26,7 @@ function App() {
       <section>
         <img src="/banner.png" alt="" />
       </section>
+      <FormularioDeEvento />
     </main>
   )
 }
